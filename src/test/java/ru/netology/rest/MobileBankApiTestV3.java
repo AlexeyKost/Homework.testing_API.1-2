6ff16e16abc91ab88bf81198fa745cbe0aaf8c88
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-05-03 23:45:25 +0300

    Update MobileBankApiTestV3.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,7 +24,7 @@ class MobileBankApiTestV3 {
                 // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[1].currency", equalTo("USD"))
+                .body("[1].currency", equalTo("USt"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
