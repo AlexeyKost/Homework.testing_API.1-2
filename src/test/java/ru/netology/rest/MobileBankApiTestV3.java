a3a0c2253c55d0114f94bfe60ab31972c88d5b82
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-05-04 21:51:59 +0300

    Add files via upload

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,28 +5,26 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
-                .when()
-                .get("/demo/accounts")
-                // Проверки
-                .then()
-                .statusCode(200)
-                // специализированные проверки - лучше
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[1].currency", equalTo("USt"))
-                .body("[0].balance", greaterThanOrEqualTo(0))
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+          // специализированные проверки - лучше
+          .contentType(ContentType.JSON)
+          .body("", hasSize(3))
+          .body("[0].currency", equalTo("RUB"))
+          .body("[0].balance", greaterThanOrEqualTo(0))
+      ;
     }
 }
